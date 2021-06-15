be6b27e7fe4e04f8aa65daced6a7fe00aa13d158
gargiscode <gargi.pattanayak@gmail.com>
2021-06-15 14:59:21 -0700

    Add project files.

---
+++ Model/Song.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicLibrary.Model
+{
+    public enum SongArtist
+    {
+        Taylor,
+        Selena,
+        Beyonce
+    }
+/*
+    public enum SongAlbum
+    {
+        Album1,
+        Album2,
+        Album3
+    }
+*/
+    public class Song
+    {
+        public string SongTitle { get; set; }
+        public SongArtist Artist { get; set; }
+        string AudioFile { get; set; }
+
+        public Song(string songTitle, SongArtist artist) 
+        {
+            SongTitle = songTitle;
+            Artist = artist;
+            AudioFile = $"Assets/Audio/{Artist}/{SongTitle}.wav";
+        }
+    }
+
+
+
+
+    
+}
